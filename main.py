9e7f8da42bef4bb05055f71ae7bd0b14076a812e
Jakub Gielniak <jbgielniak@gmail.com>
2023-04-14 21:57:12 +0200

    Optimized

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-numstr = "12345389"
+numstr = "123456788"
 
 def exceed(number):
     if len(number) > 9:
         return False
 
 def numcheck(number):
-    n=""
     for i in range(len(number)):
-        if i == 0:
-            n += number[0]
-        else:
-            for j in range(len(n)):
-                if number[i] == n[j]:
+        for j in range(len(number)):
+            if i != j:
+                if number[i] == number[j]:
                     return False
-            n+=number[i]
 
 if exceed(numstr) == False:
     print("Number exceeded its length!")
